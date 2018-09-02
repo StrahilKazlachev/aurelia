1b19db00779b3c5da74d78f1bb6adb52124048dd
bigopon.777@gmail.com <bigopon.777@gmail.com>
2018-09-02 17:46:02 +1000

    temp state

--- packages/examples/fractals/src/app.ts
+++ packages/examples/fractals/src/app.ts
@@ -1,19 +1,16 @@
-
-import { ArrayObserver, customElement } from '@aurelia/runtime';
+// tslint:disable
+import { select as d3select, mouse as d3mouse } from 'd3-selection';
+import { scaleLinear } from 'd3-scale';
 import * as view from 'text!./app.html';
-
-let id = 0;
-class Todo {
-  public done: boolean = false;
-  public id: number = id++;
-  constructor(public description: string, public app: App) {}
-}
-
-type ObservedTodos = Todo[] & { $observer: ArrayObserver };
+import { customElement } from '@aurelia/runtime';
+// import { Pythagoras } from './pythagoras';
 
 @customElement({
   name: 'app',
-  templateOrNode: <any>view,
+  templateOrNode: view as any,
+  // dependencies: [
+  //   Pythagoras
+  // ],
   build: {
     required: true,
     compiler: 'default'
@@ -21,66 +18,84 @@ type ObservedTodos = Todo[] & { $observer: ArrayObserver };
   instructions: []
 })
 export class App {
-  public log: boolean = false;
-  public count: number = 1;
-  public description: string = 'Hello World';
-  public todos: ObservedTodos = <any>[];
-
-  public addTodo(): void {
-    for (let i = 0; i < this.count; ++i) {
-      this.todos.push(new Todo(this.description, this));
+  unlimited: boolean = false;
+  //  @observable()
+  // throttleValue: number = 0;
+  width = Math.min(window.innerWidth, 1280);
+  height = this.width * (600 / 1280);
+  currentMax = 0;
+  baseW = 80;
+  heightFactor = 0;
+  lean = 0;
+  realMax = 11;
+  public readonly svg: SVGSVGElement;
+  constructor(
+  ) {
+  }
+  attached() {
+    this.next();
+    d3select(this.svg).on('mousemove', this.onMousemove);
+  }
+  private next = () => {
+    if (this.currentMax < this.realMax) {
+      this.currentMax++;
+      setTimeout(this.next, 500);
     }
   }
-
-  public clearTodos(): void {
-    this.todos.splice(0, this.todos.length);
-  }
-
-  public toggleTodos(): void {
-    for (const todo of this.todos) {
-      todo.done = !todo.done;
+  private onMousemove: Function = () => {
+    let [x, y] = d3mouse(this.svg);
+      let scaleFactor = scaleLinear()
+      .domain([this.height, 0])
+      .range([0, .8]);
+      let scaleLean = scaleLinear()
+      .domain([0, this.width / 2, this.width])
+      .range([.5, 0, -.5]);
+      this.heightFactor = scaleFactor(y);
+    this.lean = scaleLean(x);
+  };
+   throttleValueChanged(value: number) {
+    if (this.svg) {
+      d3select(this.svg)
+        .on('mousemove', null)
+        .on('mousemove',
+          !value
+            ? this.onMousemove
+            : throttle(this.onMousemove, value)
+        );
     }
-  }
-
-  public bound(): void {
-    Promise.resolve().then(() => {
-      this.todos.$observer.subscribe(this);
-      this.todos.$observer.subscribeBatched(this);
-    });
-    console.log('app bound');
-  }
-
-  public handleChange(origin: string, args?: IArguments): void {
-    if (this.log) {
-      console.log('handleChange', origin, args);
-    }
-  }
-
-  public handleBatchedChange(indexMap: Array<number>): void {
-    if (this.log) {
-      console.log('handleBatchedChange', indexMap);
-    }
-  }
-
-  public attaching(): void {
-    console.log('app attaching');
-  }
-
-  public attached(): void {
-    console.log('app attached');
-  }
-
-  public detaching(): void {
-    console.log('app detaching');
-  }
-
-  public detached(): void {
-    console.log('app detached');
-  }
-
-  public unbound(): void {
-    console.log('app unbound');
-    this.todos.$observer.unsubscribe(this);
-    this.todos.$observer.unsubscribeBatched(this);
   }
 }
+// tslint:disable
+export function throttle(func: Function, wait: number, options?: { leading: boolean; trailing: boolean }) {
+  let context, args, result;
+  let timeout = null;
+  let previous = 0;
+  if (!options) {
+    options = {} as any;
+  }
+  const later = function() {
+    previous = options.leading === false ? 0 : Date.now();
+    timeout = null;
+    result = func.apply(context, args);
+    if (!timeout) context = args = null;
+  };
+  return function() {
+    const now = Date.now();
+    if (!previous && options.leading === false) previous = now;
+    const remaining = wait - (now - previous);
+    context = this;
+    args = arguments;
+    if (remaining <= 0 || remaining > wait) {
+      if (timeout) {
+        clearTimeout(timeout);
+        timeout = null;
+      }
+      previous = now;
+      result = func.apply(context, args);
+      if (!timeout) context = args = null;
+    } else if (!timeout && options.trailing !== false) {
+      timeout = setTimeout(later, remaining);
+    }
+    return result;
+  };
+};
--- packages/examples/fractals/src/startup.ts
+++ packages/examples/fractals/src/startup.ts
@@ -1,9 +1,27 @@
+/// <reference path="./html.d.ts"/>
 import { DebugConfiguration } from '@aurelia/debug';
 import { BasicConfiguration } from '@aurelia/jit';
+import { IContainer } from '@aurelia/kernel';
 import { Aurelia } from '@aurelia/runtime';
+import { register } from '@aurelia/plugin-svg';
 import { App } from './app';
+import { Pythagoras } from './pythagoras';
 
-window['au'] = new Aurelia()
-  .register(BasicConfiguration, DebugConfiguration)
-  .app({ host: document.querySelector('app'), component: new App() })
-  .start();
+console.log({ App, Pythagoras });
+
+const au = window['au'] = new Aurelia()
+  .register(
+    BasicConfiguration,
+    DebugConfiguration,
+    // Pythagoras as any,
+    {
+      register(container: IContainer) {
+        register(container);
+        (Pythagoras as any).register(container);
+      }
+    }
+  )
+  .app({ host: document.querySelector('app'), component: new App() });
+
+au.register(<any>Pythagoras);
+au.start();
